3b9c974e74647740e95a7c859cff726ac85754d8
ianwambaire <ian.wambaire@strathmore.edu>
2024-10-05 21:34:51 +0300

    error handling

--- db_connection.php
+++ db_connection.php
@@ -8,10 +8,13 @@ class Database {
     public function getConnection() {
         $this->conn = null;
         try {
+            
             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connection successful";
         } catch(PDOException $exception) {
-            echo "Connection error: " . $exception->getMessage();
+            
+            echo "Connection error: " . $exception->getMessage() . "<br>";
         }
         return $this->conn;
     }
